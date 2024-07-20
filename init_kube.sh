5f1b9502be0fb5581edddd29087095a369301f5f
Somebody <tangming2010@126.com>
2024-07-20 22:02:19 +0800

    Update init_kube.sh

--- init_kube.sh
+++ init_kube.sh
@@ -4,9 +4,9 @@ echo "export TERM=xterm-256color" >> /root/.bashrc
 echo "source /usr/share/bash-completion/bash_completion" >> /root/.bashrc
 echo 'source <(kubectl completion bash)' >> /root/.bashrc
 echo 'complete -F __start_kubectl k' >> /root/.bashrc
-echo "alias cls=clear kc=kubectl kcsys='kubectl -n kube-system' kcr='kubectl -n radiance'"
 echo "Welcome to Radiance web terminal, try kubectl --help."
 echo 'source /opt/kubectl-aliases/.kubectl_aliases' >> /root/.bashrc
+echo "alias cls=clear kc=kubectl kcsys='kubectl -n kube-system' kcr='kubectl -n radiance'"  >> /root/.bashrc
 rm -rf /root/.kube
 mkdir -p /root/.kube
 echo -e "$1" | envsubst  > /root/.kube/config
