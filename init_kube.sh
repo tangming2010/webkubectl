2845c5e49ec4e684c20fa3f9a95f3754bc81deb0
Somebody <tangming2010@126.com>
2024-07-22 09:36:53 +0800

    Update init_kube.sh

--- init_kube.sh
+++ init_kube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "export TERM=xterm-256color" >> /root/.bashrc
+echo "export TERM=xterm-256color" > /root/.bashrc
 echo "source /usr/share/bash-completion/bash_completion" >> /root/.bashrc
 echo 'source <(kubectl completion bash)' >> /root/.bashrc
 echo 'complete -F __start_kubectl k' >> /root/.bashrc
